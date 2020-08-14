4093f37cf287dbebf7fadcd5b8fa6e6359ee5db9
adrian <rocadavid97@gmail.com>
2020-08-13 21:47:04 -0500

    Ejercicios de setAttribute, appendchild

--- Semana6/Dia1/03-element5.js
+++ Semana6/Dia1/03-element5.js
@@ -14,6 +14,44 @@ const llenarTabla = () => {
    * 3. Agregar las columnas a la fila
    * 4. Al final de cada vuelta, agregar la fila al tbody
    */
+  covid.forEach((objPais, i) => {
+    let fila = document.createElement("tr");
+
+    let tdNro = document.createElement("td");
+    tdNro.innerText = i + 1;
+
+    let tdPais = document.createElement("td");
+    tdPais.innerText = objPais.country;
+
+    let tdInfectados = document.createElement("td");
+    tdInfectados.innerText = objPais.cases;
+
+    let tdFallecidos = document.createElement("td");
+    tdFallecidos.innerText = objPais.deaths;
+
+    let tdRecuperados = document.createElement("td");
+    tdRecuperados.innerText = objPais.recovered;
+
+    let tdAcciones = document.createElement("td");
+
+    // Creando un elemento  
+    let btnEliminar = document.createElement("button");
+    btnEliminar.innerHTML = "Eliminar";
+    tdAcciones.appendChild(btnEliminar);
+    btnEliminar.onclick = () => {
+      alert(`¿Estás seguro de eliminar ${objPais.country}?`);
+      fila.setAttribute("hidden", "hidden");
+    };
+
+    fila.appendChild(tdNro);
+    fila.appendChild(tdPais);
+    fila.appendChild(tdInfectados);
+    fila.appendChild(tdFallecidos);
+    fila.appendChild(tdRecuperados);
+    fila.appendChild(tdAcciones);
+
+    tbody.appendChild(fila);
+  });
 };
 
 llenarTabla();
